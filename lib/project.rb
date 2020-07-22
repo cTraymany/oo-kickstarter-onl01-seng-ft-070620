16be09aa0a9946e764e761c630e5162fe114e770
Chindalath Traymany <chindalatht@gmail.com>
2020-07-22 01:49:56 +0000

    Automatically backed up by Learn

---
+++ lib/project.rb
@@ -0,0 +1,21 @@
+require 'pry'
+class Project
+  attr_accessor :title
+  attr_reader :backers
+  
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+end
